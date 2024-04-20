203e1b6a70eb0e418a821516418cba7a3dcb866a
JGRDeveloper <jgroldan.dev@gmail.com>
2024-04-20 17:30:25 -0300

    add dynamic method to update cards in index

---
+++ assets/script/helper/card-data.js
@@ -0,0 +1,121 @@
+let dataCard = [
+    {
+        emprendimiento: "Mantenimiento general del hogar",
+        nombre: "Alvaro Quintero",
+        telefono: "123456789",
+        mail:"alvaroq@mail.com",
+        valoracion: 5,
+        direccion: "Calle Falsa 123",
+        detalle: "Realizo todo tipo de mantenimiento en la zona de San Miguel",
+        redes:{
+            instagram: "@alvaro_quintero",
+            facebook: "alvaro quintero",
+        }
+    },
+    {
+        emprendimiento: "Catering y comida a domicilio",
+        nombre: "Sabores Caseros",
+        telefono: "987654321",
+        mail:"saborescaseros@mail.com",
+        valoracion: 4,
+        direccion: "Calle Falsa 213",
+        detalle: "Ofrecemos deliciosas comidas caseras entregadas directamente a tu puerta.",
+        redes:{
+            instagram: "@saborescaseros",
+            facebook: "Sabores Caseros",
+        }
+    },
+    {
+        emprendimiento: "Servicio de lavandería",
+        nombre: "Lavandería Clean",
+        telefono: "456123789",
+        mail:"lavanderiaclean@mail.com",
+        valoracion: 3,
+        direccion: "Calle Falsa 132",
+        detalle: "Lavamos y planchamos tu ropa con cuidado y eficiencia.",
+        redes:{
+            instagram: "@lavanderiaclean",
+            facebook: "Lavandería Clean",
+        }
+    },
+    {
+        emprendimiento: "Clases de música",
+        nombre: "Academia Musical Armonía",
+        telefono: "789456123",
+        mail:"armonia@mail.com",
+        valoracion: 5,
+        direccion: "Calle Falsa 312",
+        detalle: "Aprende a tocar instrumentos musicales con profesores experimentados.",
+        redes:{
+            instagram: "@armoniamusical",
+            facebook: "Academia Musical Armonía",
+        }
+    },
+    {
+        emprendimiento: "Servicio de jardinería",
+        nombre: "Jardines Verdes",
+        telefono: "321654987",
+        mail:"jardinesverdes@mail.com",
+        valoracion: 4,
+        direccion: "Calle Falsa 231",
+        detalle: "Diseño, mantenimiento y cuidado de jardines para hogares y empresas.",
+        redes:{
+            instagram: "@jardinesverdes",
+            facebook: "Jardines Verdes",
+        }
+    },
+    {
+        emprendimiento: "Taller de reparación de bicicletas",
+        nombre: "BiciFix",
+        telefono: "654987321",
+        mail:"bicifix@mail.com",
+        valoracion: 4,
+        direccion: "Calle Falsa 213",
+        detalle: "Reparamos y personalizamos bicicletas para que vuelvas a rodar por la ciudad.",
+        redes:{
+            instagram: "@bicifix",
+            facebook: "BiciFix",
+        }
+    },
+    {
+        emprendimiento: "Tutoría y apoyo escolar",
+        nombre: "EducaTutores",
+        telefono: "987321654",
+        mail:"educatutores@mail.com",
+        valoracion: 5,
+        direccion: "Calle Falsa 321",
+        detalle: "Brindamos clases particulares y apoyo en todas las materias escolares.",
+        redes:{
+            instagram: "@educatutores",
+            facebook: "EducaTutores",
+        }
+    },
+    {
+        emprendimiento: "Servicio de fotografía profesional",
+        nombre: "Instantáneas Creativas",
+        telefono: "147258369",
+        mail:"instantaneas@mail.com",
+        valoracion: 4,
+        direccion: "Calle Falsa 231",
+        detalle: "Capturamos tus momentos especiales con creatividad y profesionalismo.",
+        redes:{
+            instagram: "@instantaneascreativas",
+            facebook: "Instantáneas Creativas",
+        }
+    },
+    {
+        emprendimiento: "Taller de carpintería y mueblería",
+        nombre: "Maderarte",
+        telefono: "369258147",
+        mail:"info@maderarte.com",
+        valoracion: 4,
+        direccion: "Calle Falsa 312",
+        detalle: "Creamos muebles a medida y realizamos reparaciones de carpintería con materiales de alta calidad.",
+        redes:{
+            instagram: "@maderarte",
+            facebook: "Maderarte",
+        }
+    }
+]
+
+export default dataCard
--- assets/script/index.js
+++ assets/script/index.js
@@ -1,7 +1,50 @@
 import {handlerDOM} from './helper/menu.js';
+import dataCard from './helper/card-data.js';
+
+const dynamicUpdateCard = () =>{
+    const cardContainer = document.querySelector('.card-container')
+
+    dataCard.map( ({emprendimiento, nombre, telefono, mail,valoracion, direccion, redes}) => {
+        const ratingStars = '⭐'.repeat(valoracion)
+        let redesHTML = redesValidation(redes)
+        const cardHTML = generateCardHTML(redesHTML,ratingStars, {emprendimiento, nombre, telefono, mail, direccion})
+
+        cardContainer.innerHTML += cardHTML
+    })
+}
+
+const redesValidation = (redes) =>{
+    if (redes) {
+        return `
+            <p style="color:#626567">Redes Sociales</p>
+            <ul style="color:#626567">
+                ${redes.instagram ? `<li>Instagram: ${redes.instagram}</li>` : '<li style="color:white"> - </li>'}
+                ${redes.facebook ? `<li>Facebook: ${redes.facebook}</li>` : '<li style="color:white"> -</li>'}
+                <!-- Agrega más redes sociales aquí si es necesario -->
+            </ul>
+        `;
+    }
+}
+
+const generateCardHTML = (redesHTML, ratingStars, {emprendimiento, nombre, telefono, mail, direccion}) => {
+    return `
+    <div class="card">
+        <h3>${emprendimiento}</h3>
+        <p style="color:#626567 "> <b>Nombre:</b> ${nombre}</p>
+        <p style="color:#626567 "> <b>Whatsapp:</b> ${telefono}</p>
+        <p style="color:#626567 "> <b>Mail:</b> ${mail}</p>
+        <p style="color:#626567 "> <b>Dirección:</b> ${direccion}</p>
+        <p>${ratingStars}</p>
+        ${redesHTML}
+    </div>
+    `
+}
 
 document.addEventListener('DOMContentLoaded', ()=>{
     if(localStorage.getItem('adminLogin') == "true"){
         handlerDOM()
     }
+
+    dynamicUpdateCard()
+
 })
